c2fd515d47351ad2230aa14dde493aaad6274bde
muppidiyamini <yaminimuppidi1@gmail.com>
2026-03-16 11:04:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+window.addEventListener("scroll", function() {
+
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50)
+{
+navbar.style.boxShadow = "0 4px 20px rgba(0,0,0,0.1)";
+}
+else
+{
+navbar.style.boxShadow = "none";
+}
+
+});
+
+
+const btn = document.querySelector(".start-btn");
+
+btn.addEventListener("click", function(){
+
+alert("Invitation request feature coming soon!");
+
+});
